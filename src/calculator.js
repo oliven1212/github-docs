2da09e67edd4b6931525227a023dc5d5aa45f69b
Valdemar <pizza27@live.dk>
2026-03-24 09:36:07 +0100

    Update calculator.js

--- src/calculator.js
+++ src/calculator.js
@@ -8,23 +8,46 @@ function add(a, b) {
     return a + b;
 }
 
-
+/**
+ * Adds two numbers together.
+ * @param {number} a - The first number
+ * @param {number} b - The second number
+ * @returns {number} a - b
+ */
 function subtract(a, b) {
     return a - b;
 }
 
-
+/**
+ * Adds two numbers together.
+ * @param {number} a - The first number
+ * @param {number} b - The second number
+ * @returns {number} a * b
+ */
 function multiply(a, b) {
     return a * b;
 }
 
-
+/**
+ * Adds two numbers together.
+ * @param {number} a - The first number
+ * @param {number} b - The second number
+ * @returns {number} a / b
+ * @throws Argument b must be non-zero.
+ */
 function divide(a, b) {
     if (b === 0) throw new Error("Cannot divide by zero");
     return a / b;
 }
 
-
+/**
+ * 
+ * @param {number} value1 - The first number
+ * @param {number} value2 - The second number
+ * @param {"add" | "multiply"} operationType - The operation type
+ * @param {boolean} hasDiscount - To apply a discount or not
+ * @returns {number} The calculated value
+ */
 function calculateWithDiscount(value1, value2, operationType, hasDiscount) {
     let result;
 
